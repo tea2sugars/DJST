0e157742fdbd5b62f10af3905c9a8da7f1678865
Rik Barwick <rik@creativitea.co.uk>
2014-02-05 11:34:47 +0000

    Clone of OAC Site setup

---
+++ public/sites/default/files/js/js_jTldonuEHD5OSm2YLWVpQSxkq5EkXm9c3MMsvaU57xg.js
@@ -0,0 +1,249 @@
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($, Drupal) {
+  /*global jQuery:false */
+  /*global Drupal:false */
+  "use strict";
+
+  /**
+   * Provide vertical tab summaries for Bootstrap settings.
+   */
+  Drupal.behaviors.bootstrapSettingSummaries = {
+    attach: function (context) {
+      var $context = $(context);
+
+      // Components.
+      $context.find('#edit-components').drupalSetSummary(function () {
+        var summary = [];
+        // Breadcrumbs.
+        var breadcrumb = parseInt($context.find('select[name="bootstrap_breadcrumb"]').val(), 10);
+        if (breadcrumb) {
+          summary.push(Drupal.t('Breadcrumbs'));
+        }
+        // Navbar.
+        var navbar = 'Navbar: ' + $context.find('select[name="bootstrap_navbar_position"] :selected').text();
+        if ($context.find('input[name="bootstrap_navbar_inverse"]').is(':checked')) {
+          navbar += ' (' + Drupal.t('Inverse') + ')';
+        }
+        summary.push(navbar);
+        return summary.join(', ');
+      });
+
+      // Javascript.
+      $context.find('#edit-javascript').drupalSetSummary(function () {
+        var summary = [];
+        if ($context.find('input[name="bootstrap_anchors_fix"]').is(':checked')) {
+          summary.push(Drupal.t('Anchors'));
+        }
+        if ($context.find('input[name="bootstrap_popover_enabled"]').is(':checked')) {
+          summary.push(Drupal.t('Popovers'));
+        }
+        if ($context.find('input[name="bootstrap_tooltip_enabled"]').is(':checked')) {
+          summary.push(Drupal.t('Tooltips'));
+        }
+        return summary.join(', ');
+      });
+
+      // Advanced.
+      $context.find('#edit-advanced').drupalSetSummary(function () {
+        var summary = [];
+        // BootstrapCDN.
+        var bootstrapCDN = $context.find('select[name="bootstrap_cdn"]').val();
+        if (bootstrapCDN.length) {
+          bootstrapCDN = 'BootstrapCDN v' + bootstrapCDN;
+          // Bootswatch.
+          if ($context.find('select[name="bootstrap_bootswatch"]').val().length) {
+            bootstrapCDN += ' (' + $context.find('select[name="bootstrap_bootswatch"] :selected').text() + ')';
+          }
+          summary.push(bootstrapCDN);
+        }
+        // Rebuild registry.
+        if ($context.find('input[name="bootstrap_rebuild_registry"]').is(':checked')) {
+          summary.push(Drupal.t('Rebuild Registry'));
+        }
+        return summary.join(', ');
+      });
+    }
+  };
+
+  /**
+   * Provide Bootstrap Bootswatch preview.
+   */
+  Drupal.behaviors.bootstrapBootswatchPreview = {
+    attach: function (context) {
+      var $context = $(context);
+      var $preview = $context.find('#bootswatch-preview');
+      $preview.once('bootswatch', function () {
+        $.get("http://api.bootswatch.com/3/", function (data) {
+          var themes = data.themes;
+          for (var i = 0, len = themes.length; i < len; i++) {
+            $('<a/>').attr({
+              id: themes[i].name.toLowerCase(),
+              class: 'bootswatch-preview element-invisible',
+              href: themes[i].preview,
+              target: '_blank'
+            }).html(
+              $('<img/>').attr({
+                src: themes[i].thumbnail,
+                alt: themes[i].name
+              })
+            )
+            .appendTo($preview);
+          }
+          $preview.parent().find('select[name="bootstrap_bootswatch"]').bind('change', function () {
+            $preview.find('.bootswatch-preview').addClass('element-invisible');
+            if ($(this).val().length) {
+              $preview.find('#' + $(this).val()).removeClass('element-invisible');
+            }
+          }).change();
+        }, "json");
+      });
+    }
+  };
+
+  /**
+   * Provide Bootstrap navbar preview.
+   */
+  Drupal.behaviors.bootstrapNavbarPreview = {
+    attach: function (context) {
+      var $context = $(context);
+      var $preview = $context.find('#edit-navbar');
+      $preview.once('navbar', function () {
+        var $body = $context.find('body');
+        var $navbar = $context.find('#navbar.navbar');
+        $preview.find('select[name="bootstrap_navbar_position"]').bind('change', function () {
+          var $position = $(this).find(':selected').val();
+          $navbar.removeClass('navbar-fixed-bottom navbar-fixed-top navbar-static-top container');
+          if ($position.length) {
+            $navbar.addClass('navbar-'+ $position);
+          }
+          else {
+            $navbar.addClass('container');
+          }
+          // Apply appropriate classes to body.
+          $body.removeClass('navbar-is-fixed-top navbar-is-fixed-bottom navbar-is-static-top');
+          switch ($position) {
+            case 'fixed-top':
+              $body.addClass('navbar-is-fixed-top');
+              break;
+
+            case 'fixed-bottom':
+              $body.addClass('navbar-is-fixed-bottom');
+              break;
+
+            case 'static-top':
+              $body.addClass('navbar-is-static-top');
+              break;
+          }
+        });
+        $preview.find('input[name="bootstrap_navbar_inverse"]').bind('change', function () {
+          $navbar.toggleClass('navbar-inverse navbar-default');
+        });
+      });
+    }
+  };
+
+})(jQuery, Drupal);
+;
